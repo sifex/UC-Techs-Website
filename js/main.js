33b20c9b2fdd79039e52dd4cf7050d2978c4139d
Alex Sinnott <chess2ryme@gmail.com>
2016-03-24 14:08:11 +1100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+	
+
+	/*  Foundation Init    */
+	$(document).foundation();
+
+
+
+	/*  carousel Init    */
+	$('#carousel').carouFredSel({
+	 	width : '670',
+	 	pagination  : ".pagination",
+	 	responsive : true,
+	 	auto :{
+	 		duration : 900,
+	 		pauseOnHover : true
+	 	},
+	 	scroll :{
+	 		fx : 'fade'
+	 	},
+	 	items :{
+	 		visible : 1,
+	 		width : '670'
+	 	},
+		swipe: {
+			onMouse: false,
+			onTouch: true
+		}
+    });
+
+
+
+
+
+	/*    Mean navigation menu scroll to    */
+    $('a#scroll').click(function(e){
+    	e.preventDefault();
+    	scrollTo($(this).attr('href'), 900, 'easeInOutCubic');
+    });
+
+
+
+
+
+
+    /*    Back to top button    */
+    var back_top = $('#back_top');
+
+    back_top.click(function(e){
+    	e.preventDefault();
+    	scrollTo(0, 900, 'easeInOutCubic');
+    	
+    });
+
+    function scrollTo(target, speed, ease){
+    	$(window).scrollTo(target, speed, {easing:ease});
+    }
+
+    $(window).on('scroll', function(){    
+	    if($(this).scrollTop()>749)
+	    {
+	    	back_top.stop().animate({opacity : 1}, 250);
+	    }else
+	    {
+	    	back_top.stop().animate({opacity : 0}, 250);	    
+	    }   
+    });
+
+});
